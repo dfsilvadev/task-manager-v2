dce7ca006af829dadc94e2406044f52a5a86c9f2
Daniel Silva <dfsilva.dxp@gmail.com>
2024-11-01 09:14:28 -0300

    feat: add title

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import svgr from "vite-plugin-svgr"
-import * as path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +12,4 @@ export default defineConfig({
     },
   },
   base: "/",
-})
+});
