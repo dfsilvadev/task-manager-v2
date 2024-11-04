10d37423ed224edc674ba7ef2a67f63a33231c16
Daniel Silva <dfsilva.dxp@gmail.com>
2024-11-04 18:43:52 -0300

    refactor: add task header

---
+++ src/components/Tasks/ui/Header/types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export type Dependencies = {
+  children: string;
+  icon?: ReactNode;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from "./Button";
-export { default as NavLink } from "./NavLink";
+export { default as NavLink } from "./Sidebar/ui/NavLink";
 export { default as Sidebar } from "./Sidebar";
 export { default as Tasks } from "./Tasks";
