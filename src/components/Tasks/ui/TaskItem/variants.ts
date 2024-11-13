a37ac0374d2710459fa4441bf13598155a6a53dd
Daniel Silva <dfsilva.dxp@gmail.com>
2024-11-13 07:57:13 -0300

    style: add line-through when task is completed

--- src/components/Tasks/ui/TaskItem/variants.ts
+++ src/components/Tasks/ui/TaskItem/variants.ts
@@ -1,11 +1,11 @@
 import { cva } from "class-variance-authority";
 
 export const taskItemStyles = cva(
-  "flex items-center justify-between rounded-lg bg-opacity-10 p-3 text-sm",
+  "flex items-center justify-between rounded-lg bg-opacity-10 p-3 text-sm transition",
   {
     variants: {
       status: {
-        completed: "text=[#002C2E] bg-[#00ADB5]",
+        completed: "text=[#002C2E] bg-[#00ADB5] line-through",
         in_progress: "bg-[#FFAA04] text-[#FFAA04]",
         not_started: "bg-[#9A9C9F] text-[#35383E]",
       },
